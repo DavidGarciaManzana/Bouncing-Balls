a6deff61e005d55be02b63fa0211821f65377598
David <Davidgarciama@outlook.com>
2023-02-05 10:36:42 -0600

    Simplified solution

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,21 @@
 let bouncingBall = (h: number, bounce: number, window: number): number => {
     let result: number = 0;
-    let i: number = 0;
+    let isFirstTime: number = 0;
     if (h > 0 && bounce > 0 && bounce < 1 && window < h) {
         while (h > window) {
-            i == 0 ? result++ : result += 2
+            isFirstTime == 0 ? (result++,isFirstTime++) : result += 2
             h = h * bounce;
-            i++
         }
         return result;
     } else {
         return -1;
     }
 }
-
-console.log(bouncingBall(3.0, 0.66, 1.5)); //, 3
-console.log(bouncingBall(30.0, 0.66, 1.5)); //, 15
-console.log(bouncingBall(30, 0.75, 1.5)); //, 21
-console.log(bouncingBall(30, 0.4, 10)); //, 3
-console.log(bouncingBall(-1, 0.4, 10)); //, -1
-console.log(bouncingBall(30, -.5, 10)); //, -1
-console.log(bouncingBall(30, 1.5, 10)); //, -1
-console.log(bouncingBall(30, .5, 31)); //, -1
+// console.log(bouncingBall(3.0, 0.66, 1.5)); //, 3
+// console.log(bouncingBall(30.0, 0.66, 1.5)); //, 15
+// console.log(bouncingBall(30, 0.75, 1.5)); //, 21
+// console.log(bouncingBall(30, 0.4, 10)); //, 3
+// console.log(bouncingBall(-1, 0.4, 10)); //, -1
+// console.log(bouncingBall(30, -.5, 10)); //, -1
+// console.log(bouncingBall(30, 1.5, 10)); //, -1
+// console.log(bouncingBall(30, .5, 31)); //, -1
